3c7d0ec0a630450b2dd498e2341cf5ec7bc6a9b7
Firman Nugroho <56217126+fmanid@users.noreply.github.com>
2022-04-26 11:53:06 +0700

    upload modul 10

---
+++ Modul 10/my-app/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+     apiKey: "AIzaSyDUEUH4YeYo52GnELkqKIudzew4KPOOXCM",
+     authDomain: "pbf2022-f3d76.firebaseapp.com",
+     projectId: "pbf2022-f3d76",
+     storageBucket: "pbf2022-f3d76.appspot.com",
+     messagingSenderId: "644925174590",
+     appId: "1:644925174590:web:6c99c6e4d895a87c51326f",
+     measurementId: "G-ERY810E5EN"
+   };
